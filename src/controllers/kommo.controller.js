e0c5c6bf9f8d92f3d0c1629b382b1177b5a84e7e
faustofuertes <fausto.fuertes.83@gmail.com>
2025-11-19 08:34:22 -0300

    test: checkBoxValue

--- src/controllers/kommo.controller.js
+++ src/controllers/kommo.controller.js
@@ -27,6 +27,9 @@ export async function kommoWebhook(req, res) {
       const normalized = normalizeIncomingMessage(parsed);
       const contact = await getContact(normalized.contact_id);
 
+      const lead = await getLead(normalized.element_id);
+      const checkBoxValue = getCheckboxValue(lead, 1493146);
+
       if (normalized.origin === 'waba' && whiteList.includes(contact.phone)) {
         await processKommoMessage(normalized, contact);
         console.log('--------------------------------------------------------------------------------------------------------------------------------------------------------');
